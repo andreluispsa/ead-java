39199f9f9579969bd2a6a9f39daa3ebf2ec86e0a
Andre Sa <andrepena.sa@gmail.com>
2017-07-18 20:30:14 -0300

    Exemplo do uso DO-WHILE

--- Variaveis/src/variaveis/Principal.java
+++ Variaveis/src/variaveis/Principal.java
@@ -11,18 +11,27 @@ public class Principal {
         pessoa.ser = "Humano";
         System.out.println(pessoa.ser);
         System.out.println(Pessoa.ser);
-        
+
+        //exemplo do uso do FOR
         int j = 0;
         for (int i = 0; i < 10; i++) {
             j = j + (1 * i);
             System.out.println("J = " + j);
         }
         
+        //exemplo do uso do WHILE
         int i = 2;
         while (i < 20) {
             System.out.println("i = " + i);
             i = i + 2;
         }       
+        
+        //exemplo do uso do DO-WHILE
+        i = 3;
+        do {
+            System.out.println("i = " + i);
+            i = i + 2;
+        } while (i < 30);
     }
     
 }
