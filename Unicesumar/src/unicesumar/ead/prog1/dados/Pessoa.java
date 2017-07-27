d52ad796adafa31bd494ee0e2001fdc4561a731f
Andre Sa <andre.sa@efabrika.com.br>
2017-07-27 17:52:22 -0300

    Inclusão do projeto Guanabara, refatoração do projeto Unicesumar

---
+++ Unicesumar/src/unicesumar/ead/prog1/dados/Pessoa.java
@@ -0,0 +1,21 @@
+package unicesumar.ead.prog1.dados;
+
+public class Pessoa {
+    
+    String nome;
+    int idade;
+    static String ser = "Ser Humano";
+    
+    public void andar() {
+        int distancia;
+        distancia = 10;
+        nome = "Maria";
+    
+    }
+    
+    public void falar() {
+        int distancia;
+        distancia = 10;
+        idade = 30;
+    }
+}
---
+++ Unicesumar/src/unicesumar/ead/prog1/dados/Principal.java
@@ -0,0 +1,43 @@
+package unicesumar.ead.prog1.dados;
+
+import java.util.Date;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Maria";
+        System.out.println("Nome: " + pessoa.nome);
+        System.out.println(Pessoa.ser);
+        pessoa.ser = "Humano";
+        System.out.println(pessoa.ser);
+        System.out.println(Pessoa.ser);
+
+        //exemplo do uso do FOR
+        int j = 0;
+        for (int i = 0; i < 10; i++) {
+            j = j + (1 * i);
+            System.out.println("J = " + j);
+        }
+        
+        //exemplo do uso do WHILE
+        int i = 2;
+        while (i < 20) {
+            System.out.println("i = " + i);
+            i = i + 2;
+        }       
+        
+        //exemplo do uso do DO-WHILE
+        i = 3;
+        do {
+            System.out.println("i = " + i);
+            i = i + 2;
+        } while (i < 30);
+        
+        Date data = new Date();
+        System.out.println(data.toString());
+    }
+   
+    
+}
