4a67efb8009fba65c37064dd20fe9923df58b99b
Unknown <andre.sa@efabrika.com.br>
2017-08-08 22:56:03 -0300

    Udemy Classes - Composition

---
+++ UdemyJava/Composition/src/com/br/udemy/javamasterclass/Dimensions.java
@@ -0,0 +1,26 @@
+package com.br.udemy.javamasterclass;
+
+public class Dimensions {
+
+    private int width;
+    private int height;
+    private int depth;
+
+    public Dimensions(int width, int height, int depth) {
+        this.width = width;
+        this.height = width;
+        this.depth = depth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
---
+++ UdemyJava/Composition/src/com/br/udemy/javamasterclass/Main.java
@@ -0,0 +1,9 @@
+package com.br.udemy.javamasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // pause 10:49 class 46
+    }
+}
