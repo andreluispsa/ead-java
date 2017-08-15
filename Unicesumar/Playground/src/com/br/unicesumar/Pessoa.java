4db2d70e138e593659408d8476cee7bedf976b90
Unknown <andre.sa@efabrika.com.br>
2017-08-14 22:12:21 -0300

    Udemy Classes - Encapsulation / Polymorphism & Unicesumar

---
+++ Unicesumar/Playground/src/com/br/unicesumar/Pessoa.java
@@ -0,0 +1,11 @@
+package com.br.unicesumar;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    static String planeta;
+
+    public void falar() {
+        System.out.println(nome + " possui " + idade + " de idade.");
+    }
+}
