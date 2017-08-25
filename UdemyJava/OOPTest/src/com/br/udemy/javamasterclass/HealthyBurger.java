bdc7b64a552364e5ae2f032a14df7e166ed01a64
Andre Sa <andre.sa@efabrika.com.br>
2017-08-25 18:08:08 -0300

    Udemy Classes - OOPTest

---
+++ UdemyJava/OOPTest/src/com/br/udemy/javamasterclass/HealthyBurger.java
@@ -0,0 +1,24 @@
+package com.br.udemy.javamasterclass;
+
+public class HealthyBurger extends Hamburger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy", meat, price, "Brown rye");
+    }
+
+    public addHealthyAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+
+    public addHealthyAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+
+}
