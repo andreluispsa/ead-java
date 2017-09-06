06a3a55c3c37491c6a6476039a26576f263a238c
Unknown <andre.sa@efabrika.com.br>
2017-09-05 23:01:00 -0300

    Udemy - ArrayLists Challenge

--- UdemyJava/ArrayListChallenge/src/com/br/udemy/javamasterclass/Contact.java
+++ UdemyJava/ArrayListChallenge/src/com/br/udemy/javamasterclass/Contact.java
@@ -4,7 +4,7 @@ public class Contact {
     private String name;
     private String phoneNumber;
 
-    public void Contact(String name; String phoneNumber) {
+    public Contact(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
