35b9432555fd1d9d237378b42e02ccd3480d8e8f
Unknown <andre.sa@efabrika.com.br>
2017-09-08 18:43:55 -0300

    Guanabara - Interfaces

---
+++ Guanabara/Interface/src/com/br/guanabara/ControleRemoto.java
@@ -0,0 +1,87 @@
+package com.br.guanabara;
+
+public class ControleRemoto implements Controlador {
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean isLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean isTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void desligar() {
+
+    }
+
+    @Override
+    public void abrirMenu() {
+
+    }
+
+    @Override
+    public void fecharMenu() {
+
+    }
+
+    @Override
+    public void maisVolume() {
+
+    }
+
+    @Override
+    public void menosVolume() {
+
+    }
+
+    @Override
+    public void ligarMudo() {
+
+    }
+
+    @Override
+    public void desligarMudo() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+}
