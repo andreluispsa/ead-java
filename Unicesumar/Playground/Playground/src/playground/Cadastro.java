643a8866f4a0376940bad2bb7551d1e523a2de23
Unknown <andre.sa@efabrika.com.br>
2017-09-19 23:10:38 -0300

    Unicesumar - Java Swing

---
+++ Unicesumar/Playground/Playground/src/playground/Cadastro.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package playground;
+
+/**
+ *
+ * @author andreluisp
+ */
+public class Cadastro {
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private String email;
+    private String sexo;
+    private boolean alemao;
+    private boolean espanhol;
+    private boolean ingles;
+    private boolean frances;
+    private boolean italiano;
+    private boolean japones;
+    private String motivo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public boolean isAlemao() {
+        return alemao;
+    }
+
+    public void setAlemao(boolean alemao) {
+        this.alemao = alemao;
+    }
+
+    public boolean isEspanhol() {
+        return espanhol;
+    }
+
+    public void setEspanhol(boolean espanhol) {
+        this.espanhol = espanhol;
+    }
+
+    public boolean isIngles() {
+        return ingles;
+    }
+
+    public void setIngles(boolean ingles) {
+        this.ingles = ingles;
+    }
+
+    public boolean isFrances() {
+        return frances;
+    }
+
+    public void setFrances(boolean frances) {
+        this.frances = frances;
+    }
+
+    public boolean isItaliano() {
+        return italiano;
+    }
+
+    public void setItaliano(boolean italiano) {
+        this.italiano = italiano;
+    }
+
+    public boolean isJapones() {
+        return japones;
+    }
+
+    public void setJapones(boolean japones) {
+        this.japones = japones;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+    
+    
+    
+}
